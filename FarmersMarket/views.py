6f28f59e9c34819050fafa84c93c96dfb54c77fe
Mugerwa Joseph <mugsjsp21@gmail.com>
2020-07-09 11:03:45 +0300

    Update views.py
    PythonAnyWhere Auto update.

--- FarmersMarket/views.py
+++ FarmersMarket/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
+import git
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 class HomePage(TemplateView):
@@ -19,3 +23,21 @@ class ThanksPage(TemplateView):
     """docstring for ThanksPage."""
 
     template_name = 'thanks.html'
+    
+    
+@csrf_exempt
+def update(request):
+    if request.method == "POST":
+        '''
+        pass the path of the diectory where your project will be 
+        stored on PythonAnywhere in the git.Repo() as parameter.
+        Here the name of my directory is "test.pythonanywhere.com"
+        '''
+        repo = git.Repo("andylibs.pythonanywhere.com/") 
+        origin = repo.remotes.origin
+
+        origin.pull()
+
+        return HttpResponse("Updated code on PythonAnywhere")
+    else:
+        return HttpResponse("Couldn't update the code on PythonAnywhere")
